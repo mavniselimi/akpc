63605107ff87a1359f7ec696469a2468f6207771
Abdülkadir <63400996+mavniselimi@users.noreply.github.com>
2020-12-03 11:40:12 +0300

    Delete akpc.py

--- akpc.py
+++
@@ -1,7 +0,0 @@
-import os as s
-def close():
-    """This shutdown your computer in ten seconds"""
-    s.system("shutdown -s -t 10")
-def cancel():
-    """This cancel your computer shutdown"""
-    s.system("shutdown -a")
