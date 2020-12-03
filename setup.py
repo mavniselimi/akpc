be544418126037cf559e616513af76e265e5ec9f
Abdülkadir <63400996+mavniselimi@users.noreply.github.com>
2020-12-03 18:06:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mavniselimi", # Replace with your own username
+    name="akpc", # Replace with your own username
     version="0.0.1",
     author="pc",
     author_email="ferob9124@gmail.com",
